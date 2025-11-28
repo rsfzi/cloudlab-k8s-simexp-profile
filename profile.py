1fb97275b115db7220bb3609c0ee0a6d85c56c8c
Ralf Sieger <sieger@fzi.de>
2025-11-28 13:12:36 +0100

    mount the blockstore as /storage

--- profile.py
+++ profile.py
@@ -384,7 +384,7 @@ for i in range(0,allNodesCount):
         #node.exclusive = True
         node.routable_control_ip = True            
         #node.Attribute('XEN_EXTRAFS','8')
-        bs = node.Blockstore("bs-%d" % i, "/var/lib/docker")
+        bs = node.Blockstore("bs-%d" % i, "/storage")
         bs.size = "8GB"
 
     if params.diskImage:
