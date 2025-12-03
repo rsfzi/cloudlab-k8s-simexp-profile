2fb8c6fdca128d4c0507c36897608551656ed4f6
Ralf Sieger <sieger@fzi.de>
2025-12-03 08:30:23 +0100

    added disabled rabbit install for now

--- setup-kubernetes-custom.sh
+++ setup-kubernetes-custom.sh
@@ -18,19 +18,19 @@ $SUDO apt install --no-install-recommends skopeo
 # skopeo --tls-verify=false inspect docker://node-0:5000/simexp_console
 
 echo "Creating rabbitmq service and statefulset..."
-kubectl apply -f $SRC/rabbitmq-namespace.yaml
+#kubectl apply -f $SRC/rabbitmq-namespace.yaml
 status=$?
 if [ $status -ne 0 ]; then
     echo "Error: kubectl for rabbitmq-namespace failed (exit code $status)" >&2
     exit 1
 fi
-kubectl apply -f $SRC/rabbitmq-service.yaml
+#kubectl apply -f $SRC/rabbitmq-service.yaml
 status=$?
 if [ $status -ne 0 ]; then
     echo "Error: kubectl for rabbitmq-service failed (exit code $status)" >&2
     exit 1
 fi
-kubectl apply -f $SRC/vagrant/rabbitmq-statefulset.yaml
+#kubectl apply -f $SRC/vagrant/rabbitmq-statefulset.yaml
 status=$?
 if [ $status -ne 0 ]; then
     echo "Error: kubectl for rabbitmq-statefulset failed (exit code $status)" >&2
