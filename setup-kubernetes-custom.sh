7adef69bc5f4608bd041d7632c9ef1f8eb1804b2
Ralf Sieger <sieger@fzi.de>
2025-12-03 16:12:46 +0100

    enabled node tainting

--- setup-kubernetes-custom.sh
+++ setup-kubernetes-custom.sh
@@ -18,15 +18,15 @@ $SUDO apt install --no-install-recommends skopeo
 # skopeo --tls-verify=false inspect docker://node-0:5000/simexp_console
 
 echo "Taint data nodes..."
-echo "Taint data node: node-2"
-kubectl taint nodes node-2 datanode=true:NoExecute --overwrite=true
+echo "Taint data node: node-1"
+kubectl taint nodes node-1 datanode=true:NoExecute --overwrite=true
 
 echo "Taint worker nodes..."
-NON_WORKER_COUNT=3
+NON_WORKER_COUNT=2
 read -r -a arr <<< "$NODES"
 for node in "${arr[@]: -${NON_WORKER_COUNT}}"; do
     echo "Taint worker node: $node"
-    #kubectl taint nodes $node remote=true:NoExecute --overwrite=true
+    kubectl taint nodes $node remote=true:NoExecute --overwrite=true
 done
 
 echo "Creating rabbitmq service and statefulset..."
